0c4c2850d84d5804f1076315f630d4f36efd89cd
Sahil Rajput <sahilrajput03@gmail.com>
2022-08-28 16:33:01 +0530

    up

--- skype-bot/START-raspi-HTTPS.sh
+++ skype-bot/START-raspi-HTTPS.sh
@@ -5,4 +5,3 @@ set -a
 source venv/bin/activate
 # source: https://blog.miguelgrinberg.com/post/running-your-flask-application-over-https
 flask run --cert=adhoc --host=0.0.0.0
-# FYI: `flask run --host=0.0.0.0` won't make it accessible over https.
